dfd126e2c19424708a565e176098690b25f5f31c
Ralph Palomar <rpalomar@pepper.com.au>
2020-09-08 16:23:46 +0800

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { schema, uischema } from './config';
+import { 
+  materialRenderers,
+  materialCells
+} from '@jsonforms/material-renderers';
+import { JsonForms } from '@jsonforms/react';
+import { Button } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 
 function App() {
+  const [errorMessage, setErrorMessage] = React.useState<string>();
+  const [payload, setPayload] = React.useState<object>({});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Survey on Forms Usage</h1>
+      <JsonForms
+        schema={schema}
+        uischema={uischema}
+        data={payload}
+        renderers={materialRenderers}
+        cells={materialCells}
+        onChange={({ errors, data }) => { 
+          if (errors?.length !== 0) {
+            const errorMsg = errors?.map((item) => item.dataPath + " " + item.message).reduce((acc, item) => acc + item + "; ", "");
+            setErrorMessage(errorMsg);
+          } else {
+            setErrorMessage("");
+          }
+          setPayload(data)
+        }}
+      />
+      <br/>
+      <Alert severity="error">{errorMessage}</Alert>
+      <br/>
+      <Button variant="contained" color="primary" onClick={(e) => save(errorMessage, payload)}>
+         Submit
+      </Button>
+      <br/><br/><br/><br/>
     </div>
   );
+}
+
+function save(errorMsg: any, payload: any) {
+  if (errorMsg === "") {
+    console.log("Processing...")
+    console.log(payload);
+  }
 }
 
 export default App;
