9b5da53199260f0270809640c979bed2f31827c6
Saptarshi Basu <123saptarshi.basu@gmail.com>
2020-10-01 23:37:14 +0530

    Adding custom favicon and some document formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import { vf_schema, wfb_schema, wfb_uischema } from './config';
-import { 
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import { vf_schema, wfb_schema, wfb_uischema } from "./config";
+import {
   materialRenderers,
-  materialCells
-} from '@jsonforms/material-renderers';
-import { Button, Snackbar, Menu, MenuItem } from '@material-ui/core';
-import { JsonForms } from '@jsonforms/react';
-import { postData, getAllData } from './api';
-import { UISchemaElement } from '@jsonforms/core';
+  materialCells,
+} from "@jsonforms/material-renderers";
+import { Button, Snackbar, Menu, MenuItem } from "@material-ui/core";
+import { JsonForms } from "@jsonforms/react";
+import { postData, getAllData } from "./api";
+import { UISchemaElement } from "@jsonforms/core";
 
 function App() {
   const [errorMessage, setErrorMessage] = React.useState<string>();
   const [generatedJsonSchema, setJsonSchema] = React.useState<object>({});
   const [jsonPayload, setJsonPayload] = React.useState<object>({});
-  const [generatedUISchema, setUiSchema] = React.useState<UISchemaElement>({ type: 'VerticalLayout' });
+  const [generatedUISchema, setUiSchema] = React.useState<UISchemaElement>({
+    type: "VerticalLayout",
+  });
   const [open, setOpen] = React.useState(false);
   const [defaultData, setDefaultData] = React.useState<object>({});
 
   return (
     <div className="App">
-      <MenuInApp/>
-      <br/><br/>
+      <MenuInApp />
+      <br />
+      <br />
       <h2>Form Designer</h2>
       <JsonForms
         schema={wfb_schema}
@@ -30,9 +33,11 @@ function App() {
         data={defaultData}
         renderers={materialRenderers}
         cells={materialCells}
-        onChange={({ errors, data }) => { 
+        onChange={({ errors, data }) => {
           if (errors?.length !== 0) {
-            const errorMsg = errors?.map((item) => item.dataPath + " " + item.message).reduce((acc, item) => acc + item + "; ", "");
+            const errorMsg = errors
+              ?.map((item) => item.dataPath + " " + item.message)
+              .reduce((acc, item) => acc + item + "; ", "");
             setErrorMessage(errorMsg);
           } else {
             setErrorMessage("");
@@ -41,46 +46,61 @@ function App() {
             var required: any = [];
             var uiElements: any = [];
             fields?.map((item) => {
-              item.type !== 'array' ? 
-              Object.assign(properties, {
-                [item.name]: Object.assign(
-                  item.type === 'date' || item.type === 'date-time' || item.type === 'time' ? { type: 'string', format: item.type } : { type: item.type }
-                , item.enumerations != null && item.enumerations?.length !== 0 ? { enum: item.enumerations } : {})
-              }) :
-              Object.assign(properties, {
-                [item.name]: {
-                  type: 'array',
-                  items: Object.assign({
-                    type: 'string'
-                  }, item.enumerations != null && item.enumerations?.length !== 0 ? { enum: item.enumerations } : {}),
-                  uniqueItems: true
-                }
-              });
+              item.type !== "array"
+                ? Object.assign(properties, {
+                    [item.name]: Object.assign(
+                      item.type === "date" ||
+                        item.type === "date-time" ||
+                        item.type === "time"
+                        ? { type: "string", format: item.type }
+                        : { type: item.type },
+                      item.enumerations != null &&
+                        item.enumerations?.length !== 0
+                        ? { enum: item.enumerations }
+                        : {}
+                    ),
+                  })
+                : Object.assign(properties, {
+                    [item.name]: {
+                      type: "array",
+                      items: Object.assign(
+                        {
+                          type: "string",
+                        },
+                        item.enumerations != null &&
+                          item.enumerations?.length !== 0
+                          ? { enum: item.enumerations }
+                          : {}
+                      ),
+                      uniqueItems: true,
+                    },
+                  });
               if (item.mandatory) {
                 required.push(item.name);
               }
               uiElements.push({
-                type: 'Control',
-                scope: '#/properties/' + item.name,
-                label: item.description
+                type: "Control",
+                scope: "#/properties/" + item.name,
+                label: item.description,
               });
             });
             const jsonSchema = {
-              type: 'object',
+              type: "object",
               properties: properties,
-              required: required
-            }
+              required: required,
+            };
             const uiSchema = {
               type: data.layout,
-              elements: uiElements
-            }
+              elements: uiElements,
+            };
             setJsonSchema(jsonSchema);
             setUiSchema(uiSchema);
             setJsonPayload(data);
           }
         }}
       />
-      <br/><br/>
+      <br />
+      <br />
       <h2>Preview</h2>
       <JsonForms
         schema={generatedJsonSchema}
@@ -89,32 +109,48 @@ function App() {
         renderers={materialRenderers}
         cells={materialCells}
       />
-      <br/><br/>
-      <Button variant="contained" color="primary" onClick={(e) => save(errorMessage, jsonPayload, generatedJsonSchema, generatedUISchema, setOpen, setDefaultData, setJsonSchema, setUiSchema)}>
-         SAVE
+      <br />
+      <br />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={(e) =>
+          save(
+            errorMessage,
+            jsonPayload,
+            generatedJsonSchema,
+            generatedUISchema,
+            setOpen,
+            setDefaultData,
+            setJsonSchema,
+            setUiSchema
+          )
+        }
+      >
+        SAVE
       </Button>
       <Snackbar
         anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
+          vertical: "bottom",
+          horizontal: "left",
         }}
         open={open}
         autoHideDuration={6000}
         onClose={(event) => setOpen(false)}
         message="Successfully Saved"
       />
-      <br/><br/>
+      <br />
+      <br />
     </div>
   );
 }
 
 function MenuInApp() {
-
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleClose = () => {
     setAnchorEl(null);
-  }
+  };
 
   const handleClickVisualEditor = () => {
     setAnchorEl(null);
@@ -122,27 +158,37 @@ function MenuInApp() {
       <React.StrictMode>
         <App />
       </React.StrictMode>,
-      document.getElementById('root')
+      document.getElementById("root")
     );
-  }
+  };
 
   const handleClickViewForms = () => {
     setAnchorEl(null);
-    getAllData(process.env.REACT_APP_PERSISTENCE_COLLECTION)
-    .then((response) => {
-      ReactDOM.render(
-        <React.StrictMode>
-          <ListForms data={response.data} />
-        </React.StrictMode>,
-        document.getElementById('root')
-      );
-    })
-  }
+    getAllData(process.env.REACT_APP_PERSISTENCE_COLLECTION).then(
+      (response) => {
+        ReactDOM.render(
+          <React.StrictMode>
+            <ListForms data={response.data} />
+          </React.StrictMode>,
+          document.getElementById("root")
+        );
+      }
+    );
+  };
 
   return (
     <div className="Menu">
       <h1>Web Form Builder</h1>
-      <Button variant="contained" aria-controls="simple-menu" aria-haspopup="true" onClick={(event) => { setAnchorEl(event.currentTarget) } }>OPTIONS</Button>
+      <Button
+        variant="contained"
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={(event) => {
+          setAnchorEl(event.currentTarget);
+        }}
+      >
+        OPTIONS
+      </Button>
       <Menu
         id="simple-menu"
         anchorEl={anchorEl}
@@ -155,7 +201,6 @@ function MenuInApp() {
       </Menu>
     </div>
   );
-
 }
 
 function ListForms(props: any) {
@@ -165,14 +210,15 @@ function ListForms(props: any) {
     return {
       formId: item._id,
       title: item.webFormMeta.title,
-      view: false
-    }
+      view: false,
+    };
   });
 
   return (
     <div className="App">
-      <MenuInApp/>
-      <br/><br/>
+      <MenuInApp />
+      <br />
+      <br />
       <h2>List of Web Forms</h2>
       <JsonForms
         schema={vf_schema}
@@ -181,36 +227,36 @@ function ListForms(props: any) {
         cells={materialCells}
         onChange={({ errors, data }) => {
           const filterDataByView = data?.filter((item: any) => {
-            return item.view
-          })
-          const formId = filterDataByView[0]?.formId
+            return item.view;
+          });
+          const formId = filterDataByView[0]?.formId;
           if (formId != null) {
             const filterDataById = webformsData?.filter((item: any) => {
-              return item._id === formId
-            })
+              return item._id === formId;
+            });
             if (filterDataById != null) {
               ReactDOM.render(
                 <React.StrictMode>
                   <ViewForm data={filterDataById[0]} />
                 </React.StrictMode>,
-                document.getElementById('root')
+                document.getElementById("root")
               );
             }
           }
         }}
       />
     </div>
-  )
+  );
 }
-
 
 function ViewForm(props: any) {
   const [webformsData] = React.useState(props.data);
 
   return (
     <div className="App">
-      <MenuInApp/>
-      <br/><br/>
+      <MenuInApp />
+      <br />
+      <br />
       <h2>{webformsData.webFormMeta.title}</h2>
       <JsonForms
         schema={webformsData.jsonSchema}
@@ -220,22 +266,31 @@ function ViewForm(props: any) {
         cells={materialCells}
       />
     </div>
-  )
+  );
 }
 
-function save(errorMsg: any, payload: any, jsonSchema: any, uiSchema: any, setOpen: any, setDefaultData: any, setJsonSchema: any, setUISchema: any) {
+function save(
+  errorMsg: any,
+  payload: any,
+  jsonSchema: any,
+  uiSchema: any,
+  setOpen: any,
+  setDefaultData: any,
+  setJsonSchema: any,
+  setUISchema: any
+) {
   if (errorMsg === "") {
     const webFormData = {
       webFormMeta: payload,
       jsonSchema: jsonSchema,
-      uiSchema: uiSchema
-    }
-    postData(process.env.REACT_APP_PERSISTENCE_COLLECTION, webFormData)
-      .then((response) => {
+      uiSchema: uiSchema,
+    };
+    postData(process.env.REACT_APP_PERSISTENCE_COLLECTION, webFormData).then(
+      (response) => {
         setOpen(true);
         clearForm(setJsonSchema, setUISchema, setDefaultData);
-      })
-    
+      }
+    );
   }
 }
 
--- src/config.ts
+++ src/config.ts
@@ -1,90 +1,99 @@
 export const wfb_schema = {
-    type: 'object',
-    properties: {
-      title: {
-        type: 'string',
-        maxLength: 25
-      },
-      layout: {
-        type: 'string',
-        enum: ['VerticalLayout', 'HorizontalLayout']
-      },
-      fields: {
-        type: 'array',
-        items: {
-          $ref: '#/definitions/fields'
-        }
-      }
+  type: "object",
+  properties: {
+    title: {
+      type: "string",
+      maxLength: 25,
     },
-    required: ['title','layout', 'fields'],
-    definitions: {
-      fields: {
-        type: 'object',
-        properties: {
-          name: {
-            type: 'string',
-            pattern: '^[^\\s]+[\\w][^\\s]+$'
-          },
-          description: {
-            type: 'string'
-          },
-          type: {
-            type: 'string',
-            enum: ['string', 'date', 'date-time', 'time', 'number', 'integer', 'boolean', 'array']
-          },
-          mandatory: {
-            type: 'boolean'
-          },
-          enumerations: {
-            type: 'array',
-            items: {
-              type: 'string'
-            }
-          }
+    layout: {
+      type: "string",
+      enum: ["VerticalLayout", "HorizontalLayout"],
+    },
+    fields: {
+      type: "array",
+      items: {
+        $ref: "#/definitions/fields",
+      },
+    },
+  },
+  required: ["title", "layout", "fields"],
+  definitions: {
+    fields: {
+      type: "object",
+      properties: {
+        name: {
+          type: "string",
+          pattern: "^[^\\s]+[\\w][^\\s]+$",
         },
-        required: ['name', 'type']
-      }
-    }
-  }
-  export const wfb_uischema = {
-    type: 'VerticalLayout',
-    elements: [
-      {
-        type: 'Control',
-        scope: '#/properties/title',
-        label: 'Enter title'
+        description: {
+          type: "string",
+        },
+        type: {
+          type: "string",
+          enum: [
+            "string",
+            "date",
+            "date-time",
+            "time",
+            "number",
+            "integer",
+            "boolean",
+            "array",
+          ],
+        },
+        mandatory: {
+          type: "boolean",
+        },
+        enumerations: {
+          type: "array",
+          items: {
+            type: "string",
+          },
+        },
       },
-      {
-        type: 'Control',
-        scope: '#/properties/layout',
-        label: 'Choose layout'
-      },
-      {
-        type: 'Control',
-        scope: '#/properties/fields',
-        label: 'Add form fields'
-      }
-    ]
-  }
-  export const vf_schema = {
-    type: 'array',
-    items: {
-      $ref: '#/definitions/record'
+      required: ["name", "type"],
     },
-    definitions: {
-      record: {
-        type: 'object',
-        properties: {
-          formId: {
-            type: 'string'
-          },
-          title: {
-            type: 'string'
-          },
-          view: {
-            type: 'boolean'
-          }
-        }
-      }
-    }
-  }
+  },
+};
+export const wfb_uischema = {
+  type: "VerticalLayout",
+  elements: [
+    {
+      type: "Control",
+      scope: "#/properties/title",
+      label: "Enter title",
+    },
+    {
+      type: "Control",
+      scope: "#/properties/layout",
+      label: "Choose layout",
+    },
+    {
+      type: "Control",
+      scope: "#/properties/fields",
+      label: "Add form fields",
+    },
+  ],
+};
+export const vf_schema = {
+  type: "array",
+  items: {
+    $ref: "#/definitions/record",
+  },
+  definitions: {
+    record: {
+      type: "object",
+      properties: {
+        formId: {
+          type: "string",
+        },
+        title: {
+          type: "string",
+        },
+        view: {
+          type: "boolean",
+        },
+      },
+    },
+  },
+};
